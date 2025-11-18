7d0541c6bb8e12c06730e785d34031b03fe6961c
Timur Nikitenko <st128375@student.spbu.ru>
2025-11-18 12:59:51 +0300

    Change for inline buttons

--- handlers.py
+++ handlers.py
@@ -1,9 +1,9 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command, CommandStart, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state, State, StatesGroup
-from aiogram.types import (CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message,
-                        KeyboardButton,  ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from aiogram.types import (CallbackQuery, InlineKeyboardButton, 
+                           InlineKeyboardMarkup, Message, ReplyKeyboardRemove)
 
 from lexicon import LEXICON_EN
 
@@ -15,12 +15,22 @@ def create_router(logger, test, questions, user_dict):
     class Interview(StatesGroup):
         waiting_answers = State()
 
-    button_1 = KeyboardButton(text='1')
+    button_1 = InlineKeyboardButton(
+    text="1", callback_data="1"
+    )
+    button_2 = InlineKeyboardButton(
+        text="2", callback_data="2"
+    )
+
+    # Создаем объект инлайн-клавиатуры
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2]])
+
+    """ button_1 = KeyboardButton(text='1')
     button_2 = KeyboardButton(text='2')
     
     keyboard = ReplyKeyboardMarkup(keyboard=[[button_1, button_2]],
                                    resize_keyboard=True,
-                                   one_time_keyboard=True)
+                                   one_time_keyboard=True) """
 
     @router.message(CommandStart())
     async def process_start_command(message: Message):
@@ -41,47 +51,55 @@ def create_router(logger, test, questions, user_dict):
         await message.answer(LEXICON_EN[message.text])   
 
         await ask_question(message, 0, state)
-
-    async def ask_question(message: Message, question_ind: int, state: FSMContext):
+ 
+    async def ask_question(message, question_ind: int, state: FSMContext):
         q = questions[question_ind]
         q_txt = q.question
         a_option = q.answers[0]
         b_option = q.answers[1]
 
-        await message.bot.send_message(message.chat.id, f'Question №{question_ind+1}\n\n {q_txt}\n\n 1. {a_option}\n 2. {b_option}', 
+        await message.bot.send_message(chat_id=message.chat.id, text=f'Question №{question_ind+1}\n\n {q_txt}\n\n 1. {a_option}\n 2. {b_option}', 
                                        reply_markup=keyboard) 
         await state.set_state(Interview.waiting_answers)
 
         
-    @router.message(StateFilter(Interview.waiting_answers))
-    async def take_answer(message: Message, state: FSMContext):
+    @router.callback_query(F.data.in_(["1", "2"]) and StateFilter(Interview.waiting_answers))
+    async def take_answer(callback: CallbackQuery, state: FSMContext):
 
         data = await state.get_data()
         answers = data['answers']
         curr_ind = data['current_question']
 
-        if message.text.isdigit() and int(message.text) in (1, 2):
-            answers.append(int(message.text))
+    
+        if callback.data.isdigit() and int(callback.data) in (1, 2):
+            answers.append(int(callback.data))
 
             if curr_ind + 1 < data['total_questions']:
                 await state.update_data(
                 current_question=curr_ind + 1,
                 answers=answers
                 )
-                await ask_question(message, curr_ind + 1, state)
+                await callback.answer(text='Answer is accepted!')
+                await ask_question(callback.message, curr_ind + 1, state)
 
             else:
                 result = test.evaluate(answers)
-                await message.bot.send_message(message.chat.id, 
-                                    f'Test is ended! \nYour personality type is {result}', 
-                                    reply_markup=ReplyKeyboardRemove())
+                await callback.answer(text='Answer is accepted!')
+                await callback.message.answer(
+                                    f'Test is ended! \nYour personality type is {result}\n\n'\
+                                        f'Your answers: {answers}', 
+                                    reply_markup=ReplyKeyboardRemove()
+                                    )
                 
-                user_dict[message.from_user.id] = {'type': result}
+                user_dict[callback.from_user.id] = {'type': result}
                 await state.clear()
 
         else:
             logger.info('User entered incorrect value')
-            await message.bot.send_message(message.chat.id, 
-                                    'Please enter correct value (1 or 2)')
+            await callback.message.answer(
+                                    'Please enter correct value (1 or 2)'
+                                    )
 
     return router
+
+
--- lexicon.py
+++ lexicon.py
@@ -1,12 +1,12 @@
 LEXICON_EN: dict[str, str] = {
     '/start': 'Hello! This bot can analyze your personality type in two formats:\n\n'\
-        '1. Official psychological test - you will need to answer N questions to get result\n'\
+        '1. Official psychological test - you will need to answer N questions to get result.\n'\
         '2. Text in free form - you need to write at least 20 words, ideally about yourself (feelings, habits, hobbies, etc.)\n\n'\
-        'Then you get precise result if you have taken the test and supposed result if you have send text in free form. \n' \
-        'For better precision you can use 2 formats consequently',
-    '/help': 'For taking the psychological test you need to send command \\test\n'\
+        'Then you get precise result if you have taken the test and supposed result if you have send text in free form. \n\n' \
+        'For better precision you can use 2 formats consequently.',
+    '/help': 'For taking the psychological test you need to send command \\test.\n\n'\
         'For completing analysis through free text form you need to send command \\text',
-    '/test': 'In order to complete test you will need to answer 70 questions. The test will take about 15 minutes\n' \
-    'For each of the following questions, please answer as honestly as possible.\n'\
+    '/test': 'In order to complete test you will need to answer 70 questions. The test will take about 15 minutes.\n\n' \
+    'For each of the following questions, please answer as honestly as possible.\n\n'\
         'If you have not been in a situation which it alludes to, then answer as you believe you would act.'
 }
